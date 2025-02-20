d00c8d65c8437a1d0366e33d555cca16367538cc
kaushal Kumar <151421895+kaushalk0014@users.noreply.github.com>
2025-02-20 11:21:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary number to decimal number/binary-number-to-decimal-number.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.binaryToDecimal(str);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int binaryToDecimal(String b) {
+       char ch[] = b.toCharArray();
+       
+       int count=1;
+       int sum=0;
+       for(int i= ch.length-1; i>=0; i--){
+           if(ch[i]=='1'){
+               sum= sum+count;
+           }
+           count=count*2;
+       }
+       return sum;
+    }
+}
