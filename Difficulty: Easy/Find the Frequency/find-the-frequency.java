5c24e7525897d1ac5ac1e9e06a871217e1cbcb66
kaushal Kumar <151421895+kaushalk0014@users.noreply.github.com>
2025-03-02 19:50:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int x = Integer.parseInt(sc.nextLine());
+            int ans = ob.findFrequency(arr, x);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int findFrequency(int arr[], int x) {
+         
+         int count=0;
+         for(int i=0; i< arr.length; i++){
+             
+             if(arr[i] == x ){
+                 count++;
+             }
+         }
+         
+         return count;
+    }
+}
